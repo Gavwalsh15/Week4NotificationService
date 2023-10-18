ff8a59b79008b607114ba7ca27f424ae98a77383
Gavin <G00402296@atu.ie>
2023-10-18 15:03:01 +0100

    Added NotificationController and all that like

---
+++ src/main/java/ie/atu/week4notificationservice/UserDetails.java
@@ -0,0 +1,14 @@
+package ie.atu.week4notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    private String email;
+    private String name;
+}
+
